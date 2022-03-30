2ec512b0f9c8c36cb9ac130acfe9aa792eaeab0a
Arya Karthik <aryashravank@gmail.com>
2022-03-30 12:40:42 -0500

    Turn Light Functionality Added

--- AuxSystemCodePart1/AuxSystemCodePart1.ino
+++ AuxSystemCodePart1/AuxSystemCodePart1.ino
@@ -1,39 +1,35 @@
-const int hazardButton = 1;
-const int brakePedal = 2;
-const int rightTurnSignal = 3;
-const int leftTurnSignal = 4;
-unsigned long currentMillis = 0;
-unsigned long previousMillis = 0;  // unsigned Long to increase amount of memory
-const long interval = 750;         // interval at which to blink (milliseconds)
+const int ButtonHazard = 1;  //Inputs
+const int BrakePedal = 2;
+const int ButtonRight = 3;
+const int ButtonLeft = 4;
+unsigned long CurrentMillis = 0;
+unsigned long IntervalStart = 0;  // unsigned Long to increase amount of memory
+const int Interval = 750;         // interval at which to blink (milliseconds)
 
 
-const int brake = 5;               // Pin connected to LEDs
-const int turnRight = 6;
-const int turnLeft = 7;
+const int LedBrake = 5;               // Pin connected to LEDs
+const int LedRight = 6;
+const int LedLeft = 7;
 
-int brakeState = 0;                // Variables to decrease digitalRead() calls
-int hazardState = 0;               //prev_variables for edge detection
-int prev_hazard = 0;
-int rightState = 0;
-int prev_right = 0;
-int leftState = 0;
-int prev_left = 0;
-int blueButton = 13;
+int BrakeState = 0;                // Variables to decrease digitalRead() calls
+int HazardState = 0;               //prev_variables for edge detection
+int RightState = 0;
+int LeftState = 0;
+
+int blueButton = 13;  //Turn Signal LEDs
 int yellowButton = 12;
 int greenButton = 11;
 
-int rightLight = LOW;             //Variables used so that we can compare the LED states below
-int leftLight = LOW;
 
 void setup() {
-  pinMode(turnRight, OUTPUT);     // Set LED pins as output
-  pinMode(turnLeft, OUTPUT);
-  pinMode(brake, OUTPUT);
+  pinMode(LedBrake, OUTPUT);     // Set LED pins as output
+  pinMode(LedRight, OUTPUT);
+  pinMode(LedLeft, OUTPUT);
 
-  pinMode(brakePedal, INPUT);       // Set break as input
-  pinMode(hazardButton, INPUT);     // Set hazard button as input
-  pinMode(rightTurnSignal, INPUT);  // Set right signal as input
-  pinMode(leftTurnSignal, INPUT);   // Set left signal as input
+  pinMode(ButtonHazard, INPUT);       // Set break as input
+  pinMode(BrakePedal, INPUT);     // Set hazard button as input
+  pinMode(ButtonRight, INPUT);  // Set right signal as input
+  pinMode(ButtonLeft, INPUT);   // Set left signal as input
 
   pinMode(blueButton, OUTPUT);
   pinMode(yellowButton, OUTPUT);
@@ -41,55 +37,83 @@ void setup() {
 }
 
 void loop() {
-  brakeState = digitalRead(brakePedal);
-  hazardState = digitalRead(hazardButton);
-
-  if (brakeState == HIGH) {
-    digitalWrite(brake, HIGH);
+  CurrentMillis = millis();
+  BrakeState = digitalRead(BrakePedal);
+  if (BrakeState == HIGH) {
+    digitalWrite(LedBrake, HIGH);
   }
   else {
-    digitalWrite(brake, LOW);
+    digitalWrite(LedBrake, LOW);
   }
-  if (hazardState != prev_hazard) {
-
-    if (hazardState == HIGH) {
-
-      previousMillis = currentMillis; //Restarts the Interval
-      allLedOn();
-      continue;
+  HazardState = digitalRead(ButtonHazard);
+  if (HazardState == HIGH) {
+    if (CurrentMillis - IntervalStart >= Interval) {
+      IntervalStart = CurrentMillis;
+      if (digitalRead(LedRight) == HIGH || digitalRead(LedLeft) == HIGH) {
+        allOff();
+      }
+      else {
+        allOn();
+      }
     }
-
-    else {
-
-      allLedOff()
-    }
+    return;
   }
   else {
-
-    if (hazardState == HIGH) {
-
-      /*Check to change the state of the button.
-        using the millis() function, we can see how
-        many milliseconds passed since the interval start
-        (previousMillis). If this is greater than our interval
-        we toggle the LED State*/
-
-      if (currentMillis - previousMillis >= interval) { //if the interval has passed
-        //restart the interval
-        previousMillis = currentMillis;
-
-        // if the LED is off turn it on and vice-versa:
-        if (ledState == LOW) {
-          ledState = HIGH;
+    RightState = digitalRead(ButtonRight);
+    if (RightState == HIGH) {
+      leftOff();
+      if (CurrentMillis - IntervalStart >= Interval) {
+        IntervalStart = CurrentMillis;
+        if (digitalRead(LedRight) == HIGH) {
+          rightOff();
         }
         else {
-          ledState = LOW;
+          rightOn();
         }
       }
     }
+    LeftState = digitalRead(ButtonLeft);
+    if (LeftState == HIGH) {
+      rightOff();
+      if (CurrentMillis - IntervalStart >= Interval) {
+        IntervalStart = CurrentMillis;
+        if (digitalRead(LedLeft) == HIGH) {
+          leftOff();
+        }
+        else {
+          leftOn();
+        }
+      }
+      return;
+    }
+    allOff();
   }
-
 }
-void allLEDOn() {
-  rightLight = HIGH;
+
+void allOn() {
+  digitalWrite(LedRight, HIGH);
+  digitalWrite(LedLeft, HIGH);
+}
+
+void allOff() {
+  digitalWrite(LedRight, LOW);
+  digitalWrite(LedLeft, LOW);
+}
+
+void rightOn() {
+  digitalWrite(LedRight, HIGH);
+  digitalWrite(LedLeft, LOW);
+}
+
+void rightOff() {
+  digitalWrite(LedRight, LOW);
+}
+
+void leftOn() {
+  digitalWrite(LedLeft, HIGH);
+  digitalWrite(LedRight, LOW);
+}
+
+void leftOff() {
+  digitalWrite(LedLeft, LOW);
 }
