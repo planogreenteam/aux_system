7b7a1b328abb16399047014e722549052ce300a0
Arya Karthik <aryashravank@gmail.com>
2022-04-01 07:46:32 -0500

    Merge Code Files Into One File

--- AuxSystemCode/AuxSystemCode.ino
+++ AuxSystemCode/AuxSystemCode.ino
@@ -1,3 +1,7 @@
+/*#include <LiquidCrystal.h> //for LCD
+ * LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+ * int LedVoltage = 8;
+ */
 const int ButtonHazard = 1;  //Inputs
 const int BrakePedal = 2;
 const int ButtonRight = 3;
@@ -34,6 +38,12 @@ void setup() {
   pinMode(blueButton, OUTPUT);
   pinMode(yellowButton, OUTPUT);
   pinMode(greenButton, OUTPUT);
+  /*
+   * lcd.begin(16, 2);
+   * // initialize serial communication at 9600 bits per second:
+   * Serial.begin(9600);
+   *  pinMode(8, OUTPUT);
+   */
 }
 
 void loop() {
@@ -56,7 +66,6 @@ void loop() {
         allOn();
       }
     }
-    return;
   }
   else {
     RightState = digitalRead(ButtonRight);
@@ -71,6 +80,7 @@ void loop() {
           rightOn();
         }
       }
+      return;
     }
     LeftState = digitalRead(ButtonLeft);
     if (LeftState == HIGH) {
@@ -90,6 +100,38 @@ void loop() {
   }
 }
 
+/* Arudino LCD exsisting code
+ * void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  int sensorValue2 = analogRead(A1);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 12v)
+  float voltage = sensorValue * (12.0 / 1023.0);
+  // Convert the voltage to current
+  float voltage2 = sensorValue2 * (12.0 / 1023.0);
+  float current2 = (voltage2 / 220) * 1000;
+  // print out the value you read:
+  // Print a message to the LCD.
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Voltage: ");
+  lcd.print(voltage);
+  lcd.print("V");
+  if(voltage <= 11.90){
+   digitalWrite(led, HIGH);
+  }
+  else{
+    digitalWrite(led, LOW);
+  }
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  lcd.print("Current: ");
+  lcd.print(current2);
+  lcd.print("mA");
+  delay(1500);
+}
+ */
 void allOn() {
   digitalWrite(LedRight, HIGH);
   digitalWrite(LedLeft, HIGH);
